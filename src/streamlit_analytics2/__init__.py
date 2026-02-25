9654e05273c42957f4551b7c8335349a6cd61163
444B <15311579+444B@users.noreply.github.com>
2026-02-25 12:44:19 +0000

    bump version

--- src/streamlit_analytics2/__init__.py
+++ src/streamlit_analytics2/__init__.py
@@ -8,5 +8,5 @@ from .state import data  # noqa: F401
 
 from .state import data as counts  # noqa: F401  # isort:skip
 
-__version__ = "0.10.5"
+__version__ = "0.10.6"
 __name__ = "streamlit_analytics2"
